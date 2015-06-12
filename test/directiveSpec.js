35f094bf8be200e1e05057af4d2bc79ee29154b9
unknown <dan.dh.lee@gmail.com>
2015-06-12 00:11:37 -0700

    Add initial go at test

---
+++ test/directiveSpec.js
@@ -0,0 +1,28 @@
+describe('optIn', function() {
+	var scope,
+		element,
+		compiled,
+		transcludedHtml,
+		html;
+
+	beforeEach(module('myApp'));
+	beforeEach(module('template.html'));
+	beforeEach(inject(function($rootScope, $compile) {
+		transcludedHtml = '<div class="brand-logo><p>Brand Logo</p></div>';
+
+		html = "<opt-in>" + transcludedHtml + "</opt-in>";
+
+		scope = $rootScope.$new();
+		compiled = $compile(html);
+		element = compiled(scope);
+		scope.$digest();
+	}));
+
+	it('should render the element correctly', function() {
+		expect(element.find('form').length).toBe(1);
+		expect(element.find('input').length).toBe(4);
+
+		// expect(5+5).toBe(10);
+	});
+
+});
